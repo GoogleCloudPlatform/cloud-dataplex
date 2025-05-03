2f92b1139ca7c1477e8c91b6588e59d78f4854ef
Daniel Holgate <danielholgate@users.noreply.github.com>
2025-05-03 02:28:06 +0200

    Update managed-connectivity/community-contributed-connectors/sql-server-connector/src/common/gcs_uploader.py
    Co-authored-by: Sereana Seim <sereanaseim@google.com>

--- managed-connectivity/community-contributed-connectors/sql-server-connector/src/common/gcs_uploader.py
+++ managed-connectivity/community-contributed-connectors/sql-server-connector/src/common/gcs_uploader.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Sends files to GCP storage."""
+"""Sends files to Cloud Storage."""
 from typing import Dict
 from google.cloud import storage
 import logging
