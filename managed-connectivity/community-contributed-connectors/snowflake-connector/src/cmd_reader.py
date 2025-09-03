3c323789bebac39090b48ccd89b56dd83fe23fe3
danielholgate <dholgate@gmail.com>
2025-09-03 23:02:46 +0000

    fixed

--- managed-connectivity/community-contributed-connectors/snowflake-connector/src/cmd_reader.py
+++ managed-connectivity/community-contributed-connectors/snowflake-connector/src/cmd_reader.py
@@ -42,25 +42,24 @@ def read_args():
 
     # Authentication arguments
     parser.add_argument("--authentication",type=str,required=False,choices=['oauth','password','key-pair'],help="Authentication method")
+    
     credentials_group = parser.add_mutually_exclusive_group()
     credentials_group.add_argument("--password_secret", type=str,help="Google Cloud Secret Manager ID for password")
     credentials_group.add_argument("--password_file", type=str,help="Path to file containing password")
-
-    privatekey_option_group = parser.add_mutually_exclusive_group()
-    privatekey_option_group.add_argument("--key_secret", type=str,help="Google Cloud Secret Manager ID of the private key")
-    privatekey_option_group.add_argument("--key_file", type=str, help="Path to private key file for Key pair authentication")
-
-    parser.add_argument("--passphrase_file",type=str,required=False,help="Path to file containing passphrase for private key file. Can also use environment variable PRIVATE_KEY_PASSPHRASE")
-    parser.add_argument("--passphrase_secret",type=str,required=False,help="Google Cloud Secret Manager ID forprivate key passphrase.")
-    
     credentials_group.add_argument("--token_file", type=str, help="Path to file containing OAUTH authentication token")
+    credentials_group.add_argument("--key_secret", type=str,help="Google Cloud Secret Manager ID of the private key")
+    credentials_group.add_argument("--key_file", type=str, help="Path to private key file")
 
-    # Output destination arguments. Generate local only, or local + to Cloud Storage bucket
+    passphrase_option_group = parser.add_mutually_exclusive_group()
+    passphrase_option_group.add_argument("--passphrase_file",type=str,required=False,help="Path to file containing passphrase for private key file. Can also use environment variable PRIVATE_KEY_PASSPHRASE")
+    passphrase_option_group.add_argument("--passphrase_secret",type=str,required=False,help="Google Cloud Secret Manager ID forprivate key passphrase.")
+    
+    # Output destination arguments: Generate local file only, or local + Cloud Storage bucket
     output_option_group = parser.add_mutually_exclusive_group()
     output_option_group.add_argument("--local_output_only",action="store_true",help="Output metadata file in local directory only" )
     output_option_group.add_argument("--output_bucket", type=str,help="Cloud Storage bucket for metadata import file. Do not include gs:// prefix")  
+    
     parser.add_argument("--output_folder", type=str, required=False,help="Folder within bucket where generated metadata import file will be written. Specify folder name only")
-
     parser.add_argument("--min_expected_entries", type=int, required=False,default=-1,help="Minimum number of entries expected in metadata file, if less entries then file gets deleted. Safety mechanism for when using Full Entry Sync metadata jobs")
     
     parsed_args = parser.parse_known_args()[0]
@@ -69,8 +68,8 @@ def read_args():
     parsed_args = validateArguments(parsed_args)
 
     # Snowflake specific authentication validation checks
-    if parsed_args.authentication == 'oauth' and parsed_args.token is None:
-        print("--token must also be supplied if using --authentication oauth")
+    if parsed_args.authentication == 'oauth' and parsed_args.token_file is None:
+        print("--token_file must also be provided if using oauth authentication")
         sys.exit(1)
     
     if parsed_args.authentication == 'key-pair' and parsed_args.key_secret is None and parsed_args.key_file is None:
@@ -78,7 +77,7 @@ def read_args():
         sys.exit(1) 
     
     if (parsed_args.authentication is None or parsed_args.authentication == 'password') and (parsed_args.password_secret is None and parsed_args.password_file is None):
-        print("--password_secret or --password_file must supplied if using password authentication")
+        print("--password_secret or --password_file must be provided if using password authentication")
         sys.exit(1)
     
     if parsed_args.key_secret is not None:
@@ -95,7 +94,7 @@ def read_args():
         try:
             parsed_args.key_secret = loadReferencedFile(parsed_args.key_file)
         except Exception as e:
-            print(f"Error retrieving from file at {parsed_args.key_file}")
+            print(f"Error retrieving file at {parsed_args.key_file}")
             raise Exception(e)
         
     if parsed_args.passphrase_file is not None:
@@ -103,7 +102,7 @@ def read_args():
         try:
             parsed_args.key_secret = loadReferencedFile(parsed_args.passphrase_file)
         except Exception as e:
-            print(f"Error retrieving from file at {parsed_args.passphrase_file}")
+            print(f"Error retrieving file at {parsed_args.passphrase_file}")
             raise Exception(e)
     
     if parsed_args.passphrase_secret is not None:
@@ -111,7 +110,7 @@ def read_args():
         try:
             parsed_args.passphrase_secrett = get_secret(parsed_args.passphrase_secret)
         except Exception as e:
-            print(f"Error retrieving from file at {parsed_args.passphrase_secret}")
+            print(f"Error retrieving file at {parsed_args.passphrase_secret}")
         raise Exception(e)
     
     return vars(parsed_args)
