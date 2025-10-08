2dc7b84a7b3569c47bafcce74b23adbf137ea48e
danielholgate <dholgate@gmail.com>
2025-10-08 20:16:05 +0000

    SQL Server

--- managed-connectivity/community-contributed-connectors/sql-server-connector/src/sqlserver_connector.py
+++ managed-connectivity/community-contributed-connectors/sql-server-connector/src/sqlserver_connector.py
@@ -77,7 +77,7 @@ class SQLServerConnector(IExternalSourceConnector):
 
     def _get_columns(self, schema_name: str, object_type: str) -> str:
         """Gets a list of columns in tables or views."""
-        return (f"SELECT t.name AS TABLE_NAME, "
+        return (f"SELECT t.table_name AS TABLE_NAME, "
                 f"c.name AS COLUMN_NAME, "
                 f"c.data_type AS DATA_TYPE, "
                 f"c.is_nullable AS IS_NULLABLE, "
