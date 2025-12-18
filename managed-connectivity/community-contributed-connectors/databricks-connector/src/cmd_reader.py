99ac7e3c1a71cf1ad98b947b0d773c346cfe2a41
Sunny Arora <sunnyar@google.com>
2025-12-18 09:50:35 +0530

    custom connector for dbx uc

---
+++ managed-connectivity/community-contributed-connectors/databricks-connector/src/cmd_reader.py
@@ -0,0 +1,56 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+from src.common.util import loadReferencedFile
+from src.common.argument_validator import validateArguments
+
+def read_args():
+    parser = argparse.ArgumentParser()
+
+    # Project arguments for basic generation of metadata entries
+    parser.add_argument("--target_project_id", type=str, required=True,
+                        help="Google Cloud Project ID metadata entries will be import into")
+    parser.add_argument("--target_location_id", type=str, required=True,
+                        help="Google Cloud region metadata will be imported into")
+    parser.add_argument("--target_entry_group_id", type=str, required=True,
+                        help="Dataplex Entry Group ID to import metadata into")
+
+    # Databricks specific arguments
+    parser.add_argument("--workspace_url", type=str, required=True, help="Databricks workspace URL")
+    parser.add_argument("--http_path", type=str, required=True, help="Databricks SQL HTTP path, if applicable")
+    parser.add_argument("--metastore", type=str, required=True, help="Databricks metastore to connect to")
+
+    # Databricks PAT token from Secret Manager
+    parser.add_argument("--password_secret", type=str, required=True, help="Secret Manager ID containing Databricks PAT")
+
+    # Output destination arguments. Generate local only, or local + to Cloud Storage bucket
+    output_option_group = parser.add_mutually_exclusive_group()
+    output_option_group.add_argument("--local_output_only",action="store_true",help="Output metadata file in local directory only" )
+    output_option_group.add_argument("--output_bucket", type=str,help="Cloud Storage bucket for metadata import file. Do not include gs:// prefix")  
+    parser.add_argument("--output_folder", type=str, required=False,help="Folder within bucket where generated metadata import file will be written. Specify folder name only")
+
+    parser.add_argument("--min_expected_entries", type=int, required=False,default=-1,help="Minimum number of entries expected in metadata file, if less entries then file gets deleted. Safety mechanism for when using Full Entry Sync metadata jobs")
+    
+    parsed_args = parser.parse_known_args()[0]
+
+    # Validate common arguments
+    parsed_args = validateArguments(parsed_args)
+
+    # Load token from Secret Manager or local path using helper
+    parsed_args.token = loadReferencedFile(parsed_args.password_secret)
+
+    return vars(parsed_args)
+
---
+++ managed-connectivity/community-contributed-connectors/databricks-connector/src/common/util.py
@@ -0,0 +1,74 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Utility convenience functions
+import sys
+from datetime import datetime
+import re
+import os
+from google.cloud import secretmanager
+
+# Loads file at a given path and returns the content as a string if path start as secret treat is as a Secret Manager reference
+def loadReferencedFile(file_path_or_secret_id) -> str:
+    # If path starts with 'secret:' prefix, treat it as a Secret Manager reference
+    if file_path_or_secret_id.startswith("secret:"):
+        secret_id = file_path_or_secret_id.replace("secret:", "")
+        try:
+            client = secretmanager.SecretManagerServiceClient()
+            name = f"{secret_id}/versions/latest"
+            response = client.access_secret_version(request={"name": name})
+            return response.payload.data.decode("UTF-8")
+        except Exception as e:
+            print(f"Error accessing Secret Manager for {secret_id}: {e}")
+            sys.exit(1)
+    else:
+        # Fallback to reading from local file
+        try:
+            with open(file_path_or_secret_id, 'r', encoding='utf-8') as f:
+                content = f.read()
+            return content
+        except Exception as e:
+            print(f"Error while reading file {file_path_or_secret_id}: {e}")
+            sys.exit(1)
+
+# Convert string to camel case - dataplex v1 property names
+def to_camel_case(text) -> str:
+    return re.sub(r"[-_]([a-zA-Z])", lambda x: x[1].upper(), text)
+
+# folder name with timestamp
+def generateFolderName(SOURCE_TYPE: str) -> str:
+    currentDate = datetime.now()
+    return f"{SOURCE_TYPE}/{currentDate.year}{currentDate.month}{currentDate.day}-{currentDate.hour}{currentDate.minute}{currentDate.second}"
+
+# True if running in container
+def isRunningInContainer() -> bool:
+    return os.environ.get("RUNNING_IN_CONTAINER", "").lower() in ("yes", "y", "on", "true", "1")
+
+
+# Returns True is file exists at given path.
+# filePath can also be comma-seperated list of multiple jar paths
+def fileExists(filepath : str) -> bool:
+
+    if "," in filepath:
+        # Split into individual file paths and check each
+        jar_list = filepath.split(",")
+        for jar_file_path in jar_list:
+            if not os.path.isfile(jar_file_path):
+                raise Exception(f"Jar file not found: {jar_file_path}")
+    else:
+        # check single file path
+        if not os.path.isfile(filepath):
+            raise Exception(f"Jar file not found: {filepath}")
+
+    return True
